55b88f49020db2035627246ddcc35ee511c77007
gioanbatan <giovannibassani@hotmail.com>
2023-04-13 15:49:29 +0200

    Add: basic structure, Photo Model, home template + fragemente, home controller, security

---
+++ src/main/java/com/finalproject/photos/model/Photo.java
@@ -0,0 +1,69 @@
+package com.finalproject.photos.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "photos")
+public class Photo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String title;
+
+    @Lob
+    @NotBlank
+    private String description;
+
+    @Lob
+    @NotBlank
+    private String url;
+
+    @NotNull
+    private Boolean visible;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Boolean getVisible() {
+        return visible;
+    }
+
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
+}
